c4c6343c42f5bfe2c5ca984060163bb26a7d81bb
taylornielson22 <53064096+taylornielson22@users.noreply.github.com>
2025-05-16 11:24:07 -0600

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -3,8 +3,8 @@ const getSlackLimits = () => ({
   blocks: 50,
 });
 const getTeamsBytesLimit = () => 27_000;
-const getGithubApiUrl = () => process.env.GITHUB_API_URL || 'https://api.github.com';
-const getGithubServerUrl = () => process.env.GITHUB_SERVER_URL || 'https://github.com';
+const getGithubApiUrl = () => process.env.HPE_GITHUB_API_URL || 'https://api.github.com';
+const getGithubServerUrl = () => process.env.HPE_GITHUB_SERVER_URL || 'https://github.com';
 
 module.exports = {
   getSlackLimits,
